a68f7c5a1012d886d7d7b56ffbf91ad0cdc304aa
Jafnee <jafnee@redapesolutions.com>
2017-10-05 12:23:27 +0800

    Add support for Typeform Embed SDK

--- tests/urls.py
+++ tests/urls.py
@@ -3,8 +3,6 @@ from __future__ import unicode_literals, absolute_import
 
 from django.conf.urls import url, include
 
-from django_typeform.urls import urlpatterns as django_typeform_urls
 
 urlpatterns = [
-    url(r'^', include(django_typeform_urls, namespace='django_typeform')),
 ]
